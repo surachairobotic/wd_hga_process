ce31012f52597a51520e86e5d2f8780eb4ced7f0
Surachai Rodsai <surachairobotic@gmail.com>
2022-06-03 19:40:03 +0700

    ...

--- wd_hga_process/test_face_opencv.py
+++ wd_hga_process/test_face_opencv.py
@@ -1,30 +1,62 @@
 import cv2, time
 
-# Load the cascade
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def main():
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# To capture video from webcam. 
-cap = cv2.VideoCapture(4)
-# To use a video file as input 
-# cap = cv2.VideoCapture('filename.mp4')
+    # To capture video from webcam. 
+    cap = cv2.VideoCapture(4)
+    # To use a video file as input 
+    # cap = cv2.VideoCapture('filename.mp4')
 
-while True:
-    t = time.time()
-    # Read the frame
-    _, img = cap.read()
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display
-    cv2.imshow('img', img)
-    # Stop if escape key is pressed
-    k = cv2.waitKey(30) & 0xff
-    if k==27:
-        break
-    print('FPS : ' + str(time.time()-t))
-# Release the VideoCapture object
-cap.release()
+    while True:
+        t = time.time()
+        # Read the frame
+        _, img = cap.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # Display
+        cv2.imshow('img', img)
+        # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k==27:
+            break
+        print('FPS : ' + str(time.time()-t))
+    # Release the VideoCapture object
+    cap.release()
+
+def list_ports():
+    """
+    Test the ports and returns a tuple with the available ports and the ones that are working.
+    """
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports) < 6: # if there are more than 5 non working ports stop the testing. 
+        camera = cv2.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+            print("Port %s is not working." %dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                working_ports.append(dev_port)
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
+                available_ports.append(dev_port)
+        dev_port +=1
+    return available_ports,working_ports,non_working_ports
+
+if __name__ == '__main__':
+    list_ports()
+    main()
+
